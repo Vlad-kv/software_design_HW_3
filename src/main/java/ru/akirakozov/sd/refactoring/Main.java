e294484918b436c5ebcdb05d2d06b59a440841bb
Киселёв Владислав <vladislav-a-k@mail.ru>
2020-10-26 20:17:18 +0300

    Запуск приложения вынесен в отдельный класс

--- src/main/java/ru/akirakozov/sd/refactoring/Main.java
+++ src/main/java/ru/akirakozov/sd/refactoring/Main.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
+import ru.akirakozov.sd.refactoring.servlet.Application;
 import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
 import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
 
@@ -16,28 +17,8 @@ import java.sql.Statement;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
-
-        Server server = new Server(8081);
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-
-        context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
-        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
-        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
-
-        server.start();
-        server.join();
+        Application app = new Application(8081, "jdbc:sqlite:test.db");
+        app.start();
+        app.join();
     }
 }
---
+++ src/main/java/ru/akirakozov/sd/refactoring/servlet/Application.java
@@ -0,0 +1,51 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Application {
+    private final Server server;
+
+    public Application(int port, String dbURL) throws Exception {
+        try (Connection c = DriverManager.getConnection(dbURL)) {
+            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
+
+        server = new Server(port);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
+        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
+        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
+
+        server.start();
+        server.join();
+    }
+
+    public void start() throws Exception {
+        server.start();
+    }
+
+    public void join() throws Exception {
+        server.join();
+    }
+
+    public void stop() throws Exception {
+        server.stop();
+    }
+}
